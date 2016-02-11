6998ba2497a2990456789e668dc6d9ac617acc2f
Michael Spiz <mike@spiz.us>
2016-02-11 13:01:38 -0800

    descriptions and janus

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-
 set -e
+
+#update OSX prefs
 ./set-defaults.sh
+
+#install ZSH antigen
 curl -L https://raw.githubusercontent.com/zsh-users/antigen/master/antigen.zsh > ~/.antigen.zsh
 
+#install homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#install hombrew packages
 brew tap Homebrew/bundle
 brew bundle --global
+
+#set up vim with janus
+curl -L https://bit.ly/janus-bootstrap | bash
