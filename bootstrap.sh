692a7af1c98827d05f2e33049806ff306273e346
Michael Spiz <mike@spiz.us>
2016-02-11 10:57:46 -0800

    global

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,4 @@ curl -L https://raw.githubusercontent.com/zsh-users/antigen/master/antigen.zsh >
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap Homebrew/bundle
-brew bundle
+brew bundle --global
