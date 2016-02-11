362d3f9d570d5e2acce6fceaed0ac0f5fed5025f
Michael Spiz <mike@spiz.us>
2016-02-11 13:16:52 -0800

    do check for brew install

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,13 +8,16 @@ echo "***Modifying OSX settings***"
 echo "***Installing ZSH antigen***"
 curl -L https://raw.githubusercontent.com/zsh-users/antigen/master/antigen.zsh > ~/.antigen.zsh
 
-echo "***Installing homebrew***"
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if test $(which brew); then
+  echo "***Brew is already installed***"
+else
+  echo "***Installing homebrew***"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
 echo "***Installing hombrew packages***"
 brew tap Homebrew/bundle
 brew bundle --global
 
-#set up vim with janus
 echo "***Installing Vim janus***"
 curl -L https://bit.ly/janus-bootstrap | bash
